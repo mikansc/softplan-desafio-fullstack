9fe92dbd8ea12c1b4af2ded42ffb14c680372a82
mikansc <michael.nsc@outlook.com>
2020-12-20 19:46:30 -0300

    test: render a logo element with testid='projectLogo'

--- frontend/src/components/Navbar/Navbar.test.js
+++ frontend/src/components/Navbar/Navbar.test.js
@@ -1,10 +1,20 @@
 /* eslint-disable react/jsx-filename-extension */
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 import Navbar from ".";
 
 describe("Navbar component", () => {
   test("should render Navbar component correctly", () => {
     render(<Navbar />);
+  });
+
+  describe("Navbar elements", () => {
+    beforeEach(() => {
+      render(<Navbar />);
+    });
+
+    test("should render a logo element with testid='projectLogo'", () => {
+      expect(screen.getByTestId("projectLogo")).toBeInTheDocument();
+    });
   });
 });
