910ca1259b5b96018f3da5d9b73d4195f710a3e8
mikansc <michael.nsc@outlook.com>
2020-12-21 22:20:23 -0300

    refactor: adjust text input styles

--- frontend/src/components/TextInput/TextInput.style.js
+++ frontend/src/components/TextInput/TextInput.style.js
@@ -6,6 +6,25 @@ const StyledInputGroup = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: space-between;
+  margin-bottom: 1em;
+
+  label {
+    color: #216b7b;
+    margin-bottom: 5px;
+  }
+
+  input,
+  textarea {
+    width: 100%;
+    max-width: 440px;
+    background-color: #fff;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 3px;
+    padding: 0.75em 1em;
+  }
+  textarea {
+    min-width: 100%;
+  }
 `;
 
 export default StyledInputGroup;
