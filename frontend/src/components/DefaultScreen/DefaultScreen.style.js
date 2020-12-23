4f3b6ea43e92056bd77edad6c4acd3943c67f12a
mikansc <michael.nsc@outlook.com>
2020-12-23 11:10:31 -0300

    refactor: create a responsive breakpoint for desktop view

--- frontend/src/components/DefaultScreen/DefaultScreen.style.js
+++ frontend/src/components/DefaultScreen/DefaultScreen.style.js
@@ -2,11 +2,13 @@ import styled from "styled-components";
 
 const StyledMain = styled.main`
   width: 100%;
-  min-height: 88vh;
-  justify-content: center;
   display: flex;
-
+  justify-content: center;
   padding-top: 2.25em;
+
+  @media screen and (min-width: 600px) {
+    min-height: 88vh;
+  }
 `;
 
 export default StyledMain;
