914a4d7248c1ae3d60db3e60e2536174f52c58f5
mikansc <michael.nsc@outlook.com>
2020-12-24 20:28:35 -0300

    refactor: change instantiation to handle new ProjectUserDTO

--- backend/src/main/java/com/michaelnsc/softproject/config/Instantiation.java
+++ backend/src/main/java/com/michaelnsc/softproject/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.michaelnsc.softproject.config;
 
 import com.michaelnsc.softproject.domain.Project;
 import com.michaelnsc.softproject.domain.User;
+import com.michaelnsc.softproject.dto.ProjectUserDTO;
 import com.michaelnsc.softproject.repository.ProjectRepository;
 import com.michaelnsc.softproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
         User triador = new User(null, "Triador de Projetos", "triador","123456","triador@sistema.com");
         User finalizador = new User(null, "Funcionário Padrão", "finalizador","123456","finalizador@sistema.com");
 
-        Project projeto1 = new Project(null, sdf.parse("21/03/2020"), null, false, "Projeto de teste", "Escreva o parecer...", triador,finalizador);
-        Project projeto2 = new Project(null, sdf.parse("22/03/2020"), sdf.parse("24/04/2020"), true, "Mais um projeto", "Eu acho que vai dar certo", triador, finalizador);
-
         userRepository.saveAll(Arrays.asList(admin, triador, finalizador));
+
+        Project projeto1 = new Project(null, sdf.parse("21/03/2020"), null, false, "Projeto de teste", "Escreva o parecer...", new ProjectUserDTO(triador), new ProjectUserDTO(finalizador));
+        Project projeto2 = new Project(null, sdf.parse("22/03/2020"), sdf.parse("24/04/2020"), true, "Mais um projeto", "Eu acho que vai dar certo", new ProjectUserDTO(triador), new ProjectUserDTO(finalizador));
+
         projectRepository.saveAll(Arrays.asList(projeto1, projeto2));
     }
 }
