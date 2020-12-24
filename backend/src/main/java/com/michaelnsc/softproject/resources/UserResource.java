3201b7ec9676230d6cf4d98ef20d95e36c3977d4
mikansc <michael.nsc@outlook.com>
2020-12-24 17:51:00 -0300

    feat: add get by id route at UserResource

--- backend/src/main/java/com/michaelnsc/softproject/resources/UserResource.java
+++ backend/src/main/java/com/michaelnsc/softproject/resources/UserResource.java
@@ -6,16 +6,15 @@ import com.michaelnsc.softproject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value="/api/v1/users")
+@RequestMapping(value = "/api/v1/users")
 public class UserResource {
 
     @Autowired
@@ -26,6 +25,13 @@ public class UserResource {
         List<User> list = userService.findAll();
         List<UserDTO> listDto = list.stream().map(UserDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
 
     }
 }
