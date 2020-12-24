7f3c7c2dfb7cc6bfe6eac3c6932d1a88f5dbb275
mikansc <michael.nsc@outlook.com>
2020-12-24 17:40:08 -0300

    feat: create findById method on UserService

--- backend/src/main/java/com/michaelnsc/softproject/services/UserService.java
+++ backend/src/main/java/com/michaelnsc/softproject/services/UserService.java
@@ -2,10 +2,12 @@ package com.michaelnsc.softproject.services;
 
 import com.michaelnsc.softproject.domain.User;
 import com.michaelnsc.softproject.repository.UserRepository;
+import com.michaelnsc.softproject.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
     public List<User> findAll(){
         return userRepository.findAll();
     };
+
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 
 }
