57ffa2a9389082d272bc7373486a5c807db42635
mikansc <michael.nsc@outlook.com>
2020-12-24 19:03:59 -0300

    feat: implement update and updateData methods

--- backend/src/main/java/com/michaelnsc/softproject/services/UserService.java
+++ backend/src/main/java/com/michaelnsc/softproject/services/UserService.java
@@ -16,22 +16,36 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return userRepository.findAll();
-    };
+    }
 
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
-    public User insert(User obj){
+    public User insert(User obj) {
         return userRepository.insert(obj);
     }
 
     public void delete(String id) {
         findById(id);
         userRepository.deleteById(id);
+    }
+
+
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return userRepository.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setDisplayName(obj.getDisplayName());
+        newObj.setEmail(obj.getEmail());
+        newObj.setUsername(obj.getUsername());
+        newObj.setPassword(obj.getPassword());
     }
 
     public User fromDTO(UserDTO objDTO) {
