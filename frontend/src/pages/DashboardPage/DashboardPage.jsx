6d570650769a37e8fd81fa87099afc19a6277c08
mikansc <michael.nsc@outlook.com>
2020-12-24 12:23:19 -0300

    refactor: remove unecessary html elements

--- frontend/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage/DashboardPage.jsx
@@ -9,39 +9,37 @@ import { Table, TableBody, TableData, TableHead } from "../../components/Table";
 import TableRow from "../../components/Table/TableRow";
 
 const data = [
-  { name: "Michael", username: "michaelnsc", role: "Admin" },
-  { name: "Gustavo", username: "gustavo2020", role: "Triador" },
-  { name: "Fernando", username: "nandosp", role: "Seilá" },
-  { name: "Almir", username: "almirsantos", role: "Triador" },
+  { name: "Michael Nascimento", username: "michaelnsc", role: "Admin" },
+  { name: "Gustavo Sampaio", username: "gustavo2020", role: "Triador" },
+  { name: "Fernando Oliveira", username: "nandosp", role: "Seilá" },
+  { name: "Almir Santos", username: "almirsantos", role: "Triador" },
 ];
 
 const DashboardPage = () => {
   const tableHeadings = ["Nome", "Usuário", "Papel", ""];
 
   return (
-    <div className="dashboard-body">
-      <ContentContainer>
-        <ContentHeader>
-          <h2>Usuários cadastrados</h2>
-          <Button type="button">Novo usuário</Button>
-        </ContentHeader>
-        <Table>
-          <TableHead headerArray={tableHeadings} />
-          <TableBody>
-            {data.map((usuario) => {
-              return (
-                <TableRow>
-                  <TableData>{usuario.name}</TableData>
-                  <TableData>{usuario.username}</TableData>
-                  <TableData>{usuario.role}</TableData>
-                  <TableData>Editar / Excluir</TableData>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </ContentContainer>
-    </div>
+    <ContentContainer>
+      <ContentHeader>
+        <h2>Usuários cadastrados</h2>
+        <Button type="button">Novo usuário</Button>
+      </ContentHeader>
+      <Table>
+        <TableHead headerArray={tableHeadings} />
+        <TableBody>
+          {data.map((usuario) => {
+            return (
+              <TableRow>
+                <TableData>{usuario.name}</TableData>
+                <TableData>{usuario.username}</TableData>
+                <TableData>{usuario.role}</TableData>
+                <TableData>Editar / Excluir</TableData>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </ContentContainer>
   );
 };
 
