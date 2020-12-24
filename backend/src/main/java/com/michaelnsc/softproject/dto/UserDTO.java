d7a93f54c3ab6e35b7b73cf33691413fcb4d95fe
mikansc <michael.nsc@outlook.com>
2020-12-24 17:31:49 -0300

    refactor: modify UserDTO to show only relevant data

--- backend/src/main/java/com/michaelnsc/softproject/dto/UserDTO.java
+++ backend/src/main/java/com/michaelnsc/softproject/dto/UserDTO.java
@@ -9,8 +9,6 @@ public class UserDTO implements Serializable {
     private String id;
     private String displayName;
     private String username;
-    private String password;
-    private String email;
 
     public UserDTO(){}
 
@@ -42,21 +40,5 @@ public class UserDTO implements Serializable {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
