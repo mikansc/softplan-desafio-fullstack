4bc2a6cc9a44e4ff4cfadbeac3969924d45eb5e6
mikansc <michael.nsc@outlook.com>
2020-12-25 18:24:32 -0300

    feat: add loadUserByUsername method

---
+++ backend/src/main/java/com/michaelnsc/softproject/security/UserSS.java
@@ -0,0 +1,64 @@
+package com.michaelnsc.softproject.security;
+
+import com.michaelnsc.softproject.domain.enums.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserSS implements UserDetails {
+
+    private String id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSS(String id, String username, String password, Set<Role> roles) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = roles.stream().map(x -> new SimpleGrantedAuthority(x.getDescription())).collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ backend/src/main/java/com/michaelnsc/softproject/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.michaelnsc.softproject.services;
+
+import com.michaelnsc.softproject.domain.User;
+import com.michaelnsc.softproject.repository.UserRepository;
+import com.michaelnsc.softproject.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User usr = userRepository.findByUsername(username);
+        if(usr == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new UserSS(usr.getId(),usr.getUsername(),usr.getPassword(),usr.getRoles());
+    }
+}
