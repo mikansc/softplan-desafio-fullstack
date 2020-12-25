dade39502c4d80a7b346ac47a6c12466c9a0e771
mikansc <michael.nsc@outlook.com>
2020-12-24 22:19:08 -0300

    feat: create ProjectService class

--- backend/src/main/java/com/michaelnsc/softproject/services/ProjectService.java
+++ backend/src/main/java/com/michaelnsc/softproject/services/ProjectService.java
@@ -1,24 +1,21 @@
 package com.michaelnsc.softproject.services;
 
-import com.michaelnsc.softproject.domain.User;
-import com.michaelnsc.softproject.dto.UserDTO;
+import com.michaelnsc.softproject.domain.Project;
 import com.michaelnsc.softproject.repository.ProjectRepository;
-import com.michaelnsc.softproject.repository.UserRepository;
 import com.michaelnsc.softproject.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
-public class PostService {
+public class ProjectService {
 
     @Autowired
     private ProjectRepository projectRepository;
 
-    public User findById(String id) {
-        Optional<User> obj = projectRepository.findById(id);
+    public Project findById(String id) {
+        Optional<Project> obj = projectRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
