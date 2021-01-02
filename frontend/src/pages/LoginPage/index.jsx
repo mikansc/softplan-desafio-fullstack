c52db9e9e2ebb5c9d9b769119469b07c22c2c9f2
mikansc <michael.nsc@outlook.com>
2021-01-02 20:48:23 -0300

    refactor: include dispatch action to login

--- frontend/src/pages/LoginPage/index.jsx
+++ frontend/src/pages/LoginPage/index.jsx
@@ -1,24 +1,50 @@
-import React from "react";
+/* eslint-disable arrow-body-style */
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import LoginContainer from "../../components/LoginContainer";
 import Button from "../../components/Button";
 import TextInput from "../../components/TextInput";
+import { login } from "../../store/userDomain/actions";
 
-const LoginPage = () => {
+const LoginPage = ({ history }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const session = useSelector((state) => state.session);
+  const { userInfo } = session;
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push("dashboard/users");
+    }
+  }, [history, userInfo]);
+
   const submitLoginHandler = (event) => {
     event.preventDefault();
-    // eslint-disable-next-line no-console
-    console.log("Login click");
+    dispatch(login(username, password));
   };
 
   return (
     <LoginContainer>
       <h2>Acesso ao sistema</h2>
       <form onSubmit={submitLoginHandler}>
-        <TextInput inputLabel="Usuário" inputType="text" inputId="username" />
-        <TextInput inputLabel="Senha" inputType="password" inputId="password" />
-        <Button submit onPress={submitLoginHandler}>
-          ENTRAR
-        </Button>
+        <TextInput
+          inputLabel="Usuário"
+          inputType="text"
+          inputId="username"
+          value={username}
+          onChange={setUsername}
+        />
+        <TextInput
+          inputLabel="Senha"
+          inputType="password"
+          inputId="password"
+          value={password}
+          onChange={setPassword}
+        />
+        <Button submit>ENTRAR</Button>
       </form>
     </LoginContainer>
   );
