4a804a17365cfb174c5b8826e6f806b957292e1e
mikansc <michael.nsc@outlook.com>
2021-01-02 18:08:25 -0300

    feat: create axios service to get user data

---
+++ frontend/src/services/AxiosUserService.js
@@ -0,0 +1,95 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080/api/v1/";
+
+class UserService {
+  constructor(url) {
+    this.axiosInstance = axios.create({
+      baseURL: url,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  login(username, password) {
+    return this.axiosInstance
+      .post(
+        "/login",
+        { username, password },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        return response.headers.authorization.split(" ")[1];
+      });
+  }
+
+  authenticated(token) {
+    return this.axiosInstance
+      .get("users/profile", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        return response.data;
+      });
+  }
+
+  listAll(token) {
+    return axios
+      .get(this.BASE_URL, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        return response.data;
+      });
+  }
+
+  create(userData, token) {
+    return axios
+      .post(this.BASE_URL, userData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        return response.data;
+      });
+  }
+
+  update(userData, userId, token) {
+    return axios
+      .put(`${this.BASE_URL}/${userId}`, userData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        return response.data;
+      });
+  }
+
+  delete(userId, token) {
+    return axios
+      .delete(`${this.BASE_URL}/${userId}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        return response.data;
+      });
+  }
+}
+
+export default new UserService(BASE_URL);
