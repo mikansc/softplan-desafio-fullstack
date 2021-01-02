1ef57c03d30d84c880d681c74e66519db62ae82e
mikansc <michael.nsc@outlook.com>
2021-01-02 20:36:22 -0300

    fix: fix onClick event handler on button component

--- frontend/src/components/Button/index.jsx
+++ frontend/src/components/Button/index.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import StyledButton from "./Button.style";
 
 const Button = (props) => {
-  const { children, submit, fullWidth, isCancel, onPress } = props;
-  const onPressHandler = () => {
-    onPress();
+  const { children, submit, fullWidth, isCancel, onButtonClick } = props;
+  const onClickHandler = () => {
+    return onButtonClick ? onButtonClick() : null;
   };
   return (
     <StyledButton
       fullWidth={fullWidth}
       isCancel={isCancel}
       type={submit ? "submit" : "button"}
-      onPress={onPressHandler}
+      onClick={onClickHandler}
     >
       {children}
     </StyledButton>
@@ -22,6 +22,7 @@ Button.defaultProps = {
   btnLabel: "Botão",
   fullWidth: false,
   isCancel: false,
+  onClick: null,
 };
 
 export default Button;
