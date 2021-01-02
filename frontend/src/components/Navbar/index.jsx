ae6abc8bd263dde6b00113bf5ef2d83bff98d6e1
mikansc <michael.nsc@outlook.com>
2021-01-02 19:18:59 -0300

    feat: include logout button to navbar

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { useDispatch } from "react-redux";
 import StyledNav from "./Navbar.style";
+import { logout } from "../../store/userDomain/actions";
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const onClickHandler = () => {
+    console.log("Logout");
+    dispatch(logout());
+  };
+
   return (
     <StyledNav>
       <div className="logo">
         <h1 data-testid="projectLogo">SIPROJ - Softplan</h1>
       </div>
-      <button type="button" className="icon">
+      <button type="button" className="icon" onClick={onClickHandler}>
         <ExitToAppIcon />
       </button>
     </StyledNav>
