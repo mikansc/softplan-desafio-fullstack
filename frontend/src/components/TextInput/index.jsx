276061715fb6242583bbe51c1dbca4c63ced2856
mikansc <michael.nsc@outlook.com>
2021-01-02 20:36:49 -0300

    refactor: prepare TextInput to handle value prop

--- frontend/src/components/TextInput/index.jsx
+++ frontend/src/components/TextInput/index.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import StyledInputGroup from "./TextInput.style";
 
 const TextInput = (props) => {
-  const { inputLabel, inputType, inputName, inputId } = props;
+  const { inputLabel, inputType, inputName, inputId, value, onChange } = props;
+
+  const handleOnChange = (e) => {
+    onChange(e.target.value);
+  };
+
   return (
     <StyledInputGroup>
       <label htmlFor={inputId}>{inputLabel}</label>
       {inputType === "textarea" ? (
-        <textarea name={inputName} id={inputId} />
+        <textarea name={inputName} id={inputId}>
+          {value}
+        </textarea>
       ) : (
-        <input type={inputType} name={inputName} id={inputId} />
+        <input
+          type={inputType}
+          name={inputName}
+          id={inputId}
+          value={value}
+          onChange={handleOnChange}
+        />
       )}
     </StyledInputGroup>
   );
