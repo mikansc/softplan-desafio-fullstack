311d32a4e68608615b71c8101ecc9b3bf916f3cb
mikansc <michael.nsc@outlook.com>
2021-01-02 22:52:40 -0300

    feat: create a simple AssignedProjectListPage for future implementation

---
+++ frontend/src/pages/projectDomain/AssignedProjectListPage/AssignedProjectListPage.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable arrow-body-style */
+import React from "react";
+import {
+  ContentContainer,
+  ContentHeader,
+} from "../../../components/ContentContainer";
+
+const AssignedProjectListPage = () => {
+  return (
+    <ContentContainer>
+      <ContentHeader>
+        <h2>Seus projetos</h2>
+      </ContentHeader>
+    </ContentContainer>
+  );
+};
+
+export default AssignedProjectListPage;
---
+++ frontend/src/pages/projectDomain/AssignedProjectListPage/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars */
+import AssignedProjectListPage from "./AssignedProjectListPage";
+
+export default AssignedProjectListPage;
