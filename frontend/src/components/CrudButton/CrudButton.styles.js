d37bb4bf346b49ae2829579e5475db5ac2d7a51b
mikansc <michael.nsc@outlook.com>
2021-01-03 12:22:53 -0300

    feat: create CRUD button component

---
+++ frontend/src/components/CrudButton/CrudButton.styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const defaultActionColors = {
+  edit: { bg: "#DFB657", txt: "#FFF" },
+  delete: { bg: "#C74231", txt: "#FFF " },
+};
+
+const StyledButton = styled.button`
+  padding: 0.35em;
+  text-transform: uppercase;
+  margin-left: 0.75em;
+  border-radius: 5px;
+
+  background-color: ${(props) => {
+    return defaultActionColors[props.actionColor].bg;
+  }};
+
+  color: ${(props) => {
+    return defaultActionColors[props.actionColor].txt;
+  }};
+
+  @media screen and (min-width: 600px) {
+    margin-left: 0.5em;
+  }
+`;
+
+export default StyledButton;
---
+++ frontend/src/components/CrudButton/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Edit from "@material-ui/icons/Edit";
+import Delete from "@material-ui/icons/Delete";
+import StyledButton from "./CrudButton.styles";
+
+const CrudButton = (props) => {
+  const { action, onClick } = props;
+  const onClickHandler = () => {
+    return onClick ? onClick() : null;
+  };
+  return (
+    <StyledButton actionColor={action} type="button" onClick={onClickHandler}>
+      {action === "edit" ? <Edit /> : <Delete />}
+    </StyledButton>
+  );
+};
+
+CrudButton.defaultProps = {
+  action: "edit",
+  onClick: null,
+};
+
+export default CrudButton;
