7beedf296f8b4c866220854a7c6792cbb8fa1739
mikansc <michael.nsc@outlook.com>
2021-01-03 19:30:21 -0300

    refactor: adjust Container component to be more configurable

--- frontend/src/components/Container/Container.style.js
+++ frontend/src/components/Container/Container.style.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 const StyledContainer = styled.div`
   display: flex;
   flex-direction: ${({ row }) => (row ? "row" : "column")};
+  justify-content: ${({ row }) => (row ? "space-between" : "center")};
+  align-items: ${({ row }) => (row ? "center" : null)};
   flex-wrap: wrap;
 
   @media screen and (min-width: 600px) {
