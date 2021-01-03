494fcfac7730f628360fd0d11c69cfb7a234d7cd
mikansc <michael.nsc@outlook.com>
2021-01-02 21:11:27 -0300

    refactor: change LoginPage to use hook useHistory instead history props

--- frontend/src/pages/LoginPage/index.jsx
+++ frontend/src/pages/LoginPage/index.jsx
@@ -1,25 +1,27 @@
 /* eslint-disable arrow-body-style */
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import LoginContainer from "../../components/LoginContainer";
 import Button from "../../components/Button";
 import TextInput from "../../components/TextInput";
 import { login } from "../../store/userDomain/actions";
 
-const LoginPage = ({ history }) => {
+const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const session = useSelector((state) => state.session);
-  const { userInfo } = session;
+  const { loading, userInfo } = session;
 
   useEffect(() => {
     if (userInfo) {
-      history.push("dashboard/users");
+      history.push("dashboard");
     }
-  }, [history, userInfo]);
+  }, [history, userInfo, loading]);
 
   const submitLoginHandler = (event) => {
     event.preventDefault();
