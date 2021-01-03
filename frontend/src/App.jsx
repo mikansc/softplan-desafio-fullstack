89481f095e3161c8655c4185b8daefe2873bbfcc
mikansc <michael.nsc@outlook.com>
2021-01-02 22:52:56 -0300

    refactor: create all routes to pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,84 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./routes/PrivateRoute";
 
+import NotFoundPage from "./pages/NotFoundPage";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
+
+import UserlistPage from "./pages/userDomain/UserlistPage";
+import NewUserPage from "./pages/userDomain/NewUserPage";
+import UserDetailsPage from "./pages/userDomain/UserDetailsPage";
+import EditUserPage from "./pages/userDomain/EditUserPage";
+
+import EditProjectPage from "./pages/projectDomain/EditProjectPage";
+import NewProjectPage from "./pages/projectDomain/NewProjectPage";
+import ProjectDetailsPage from "./pages/projectDomain/ProjectDetailsPage";
+import ProjectListPage from "./pages/projectDomain/ProjectListPage";
+import AssignedProjectListPage from "./pages/projectDomain/AssignedProjectListPage";
+import AssignedProjectDetailsPage from "./pages/projectDomain/AssignedProjectDetailsPage";
+import AssignedProjectEditPage from "./pages/projectDomain/AssignedProjectEditPage";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <Route path="/" exact component={LoginPage} />
-      <Route path="/dashboard/users" component={DashboardPage} />
+      <Switch>
+        <Route exact path="/">
+          <LoginPage />
+        </Route>
+
+        <PrivateRoute exact path="/dashboard">
+          <DashboardPage />
+        </PrivateRoute>
+
+        <PrivateRoute path="/dashboard/users">
+          <UserlistPage />
+        </PrivateRoute>
+
+        <PrivateRoute path="/dashboard/users/new">
+          <NewUserPage />
+        </PrivateRoute>
+
+        <PrivateRoute path="/dashboard/users/:id">
+          <UserDetailsPage />
+        </PrivateRoute>
+
+        <PrivateRoute path="/dashboard/users/:id/edit">
+          <EditUserPage />
+        </PrivateRoute>
+
+        <PrivateRoute exact path="/dashboard/projects">
+          <ProjectListPage />
+        </PrivateRoute>
+
+        <PrivateRoute path="/dashboard/projects/new">
+          <NewProjectPage />
+        </PrivateRoute>
+
+        <PrivateRoute path="/dashboard/projects/:id">
+          <ProjectDetailsPage />
+        </PrivateRoute>
+
+        <PrivateRoute path="/dashboard/projects/:id/edit">
+          <EditProjectPage />
+        </PrivateRoute>
+
+        <PrivateRoute exact path="/dashboard/assigned">
+          <AssignedProjectListPage />
+        </PrivateRoute>
+
+        <PrivateRoute exact path="/dashboard/assigned/:id/">
+          <AssignedProjectDetailsPage />
+        </PrivateRoute>
+
+        <PrivateRoute exact path="/dashboard/assigned/:id/edit">
+          <AssignedProjectEditPage />
+        </PrivateRoute>
+
+        <Route component={NotFoundPage} />
+      </Switch>
     </Router>
   );
 };
