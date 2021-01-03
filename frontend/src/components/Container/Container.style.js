928b9d9f237ee9e210e16e97931e0cde2b1109d8
mikansc <michael.nsc@outlook.com>
2021-01-03 17:58:53 -0300

    fix: adjust component responsiveness

--- frontend/src/components/Container/Container.style.js
+++ frontend/src/components/Container/Container.style.js
@@ -3,6 +3,11 @@ import styled from "styled-components";
 const StyledContainer = styled.div`
   display: flex;
   flex-direction: ${({ row }) => (row ? "row" : "column")};
+  flex-wrap: wrap;
+
+  @media screen and (min-width: 600px) {
+    flex-wrap: nowrap;
+  }
 `;
 
 export default StyledContainer;
