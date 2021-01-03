f72919f15668b6f34f3850c065d16aeb344269b5
mikansc <michael.nsc@outlook.com>
2021-01-02 22:32:59 -0300

    feat: create a simple UserDetailsPage for future implementation

---
+++ frontend/src/pages/UserDetailsPage/UserDetailsPage.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable arrow-body-style */
+import React from "react";
+import {
+  ContentContainer,
+  ContentHeader,
+} from "../../components/ContentContainer";
+
+const UserDetailsPage = () => {
+  return (
+    <ContentContainer>
+      <ContentHeader>
+        <h2>Detalhes do usuário</h2>
+      </ContentHeader>
+    </ContentContainer>
+  );
+};
+
+export default UserDetailsPage;
---
+++ frontend/src/pages/UserDetailsPage/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars */
+import UserDetailsPage from "./UserDetailsPage";
+
+export default UserDetailsPage;
