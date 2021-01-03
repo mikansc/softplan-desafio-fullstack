58e78dce3ecdbbebdb87899a3485c7ede5cdca0e
mikansc <michael.nsc@outlook.com>
2021-01-02 22:46:37 -0300

    feat: create a simple ProjectListPage for future implementation

---
+++ frontend/src/pages/projectDomain/ProjectListPage/ProjectListPage.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable arrow-body-style */
+import React from "react";
+import {
+  ContentContainer,
+  ContentHeader,
+} from "../../../components/ContentContainer";
+
+const ProjectListPage = () => {
+  return (
+    <ContentContainer>
+      <ContentHeader>
+        <h2>Todos os projetos</h2>
+      </ContentHeader>
+    </ContentContainer>
+  );
+};
+
+export default ProjectListPage;
---
+++ frontend/src/pages/projectDomain/ProjectListPage/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars */
+import ProjectListPage from "./ProjectListPage";
+
+export default ProjectListPage;
