e213357565dbc60f1a08ff4dbd76f4f4bedd1929
mikansc <michael.nsc@outlook.com>
2021-01-03 15:27:16 -0300

    refactor: ger user details from state

--- frontend/src/pages/UserDomainPages/UserDetailsPage/UserDetailsPage.jsx
+++ frontend/src/pages/UserDomainPages/UserDetailsPage/UserDetailsPage.jsx
@@ -1,15 +1,26 @@
-/* eslint-disable arrow-body-style */
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import {
   ContentContainer,
   ContentHeader,
 } from "../../../components/ContentContainer";
+import { getUser } from "../../../store/userDomain/actions";
 
 const UserDetailsPage = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const userDetails = useSelector((state) => state.userDetails);
+  const { user } = userDetails;
+
+  useEffect(() => {
+    dispatch(getUser(id));
+  }, [dispatch]);
+
   return (
     <ContentContainer>
       <ContentHeader>
-        <h2>Detalhes do usuário</h2>
+        <h2>Detalhes do usuário {user && user.displayName}</h2>
       </ContentHeader>
     </ContentContainer>
   );
