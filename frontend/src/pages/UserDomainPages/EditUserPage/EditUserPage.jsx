468306a7b27b2c491342a1ae7037df7d61ea5c14
mikansc <michael.nsc@outlook.com>
2021-01-04 18:22:34 -0300

    refactor: add checkbox to EditUserPage

--- frontend/src/pages/UserDomainPages/EditUserPage/EditUserPage.jsx
+++ frontend/src/pages/UserDomainPages/EditUserPage/EditUserPage.jsx
@@ -2,26 +2,26 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
 
+import { getUser, updateUser } from "../../../store/userDomain/actions";
+import parseRole from "../../../commons/utils/parseRole";
+
 import {
   ContentContainer,
   ContentHeader,
 } from "../../../components/ContentContainer";
-import { getUser } from "../../../store/userDomain/actions";
 import Button from "../../../components/Button";
 import Container from "../../../components/Container";
 import TextInput from "../../../components/TextInput";
+import CheckboxInput from "../../../components/CheckboxInput";
 
-import SelectInput from "../../../components/SelectInput";
-import parseRole from "../../../commons/utils/parseRole";
-import getUserRole from "../../../commons/utils/getUserRole";
-
-const DEFAULT_ROLES = ["ADMIN", "MANAGER", "FINISHER"];
+const DEFAULT_ROLES = ["ADMIN", "MANAGER"];
 
 const EditUserPage = () => {
   const [displayName, setDisplayName] = useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
-  const [selectedRole, setSelectedRole] = useState("");
+  const [roles, setRoles] = useState([]);
+  const [password, setPassword] = useState("");
 
   const { id } = useParams();
   const dispatch = useDispatch();
@@ -37,9 +37,17 @@ const EditUserPage = () => {
       setDisplayName(user.displayName);
       setUsername(user.username);
       setEmail(user.email);
-      setSelectedRole(getUserRole(user.roles));
+      setRoles(user.roles);
     }
   }, [dispatch, user]);
+
+  const handleSelectRole = (selectedRole) => {
+    if (roles.includes(selectedRole)) {
+      setRoles(roles.filter((role) => role !== selectedRole));
+    } else {
+      setRoles([...roles, selectedRole]);
+    }
+  };
 
   const submitFormHandler = (event) => {
     event.preventDefault();
@@ -47,9 +55,11 @@ const EditUserPage = () => {
       displayName,
       email,
       username,
-      selectedRole,
+      password,
+      roles,
     };
-    console.log(updatedUser);
+    dispatch(updateUser(updatedUser, user.id));
+    history.push("/dashboard/users");
   };
 
   const handleCancelOperation = () => {
@@ -90,25 +100,33 @@ const EditUserPage = () => {
             onInput={setEmail}
           />
         </Container>
-        <Container row>
-          {user && (
-            <SelectInput
-              inputLabel="Papel"
-              halfWidth
-              inputId="role"
-              name="role"
-              value={selectedRole}
-              onChange={setSelectedRole}
-            >
-              {user &&
-                DEFAULT_ROLES.map((role) => (
-                  <option key={role} value={role}>
-                    {parseRole(role)}
-                  </option>
-                ))}
-            </SelectInput>
-          )}
+        <Container row halfWidth>
+          {roles &&
+            DEFAULT_ROLES.map((role) => (
+              <CheckboxInput
+                key={role}
+                inputLabel={parseRole(role)}
+                type="checkbox"
+                name={role}
+                id={role}
+                value={role}
+                checked={roles.includes(role)}
+                onChange={handleSelectRole}
+              />
+            ))}
         </Container>
+        <Container>
+          <TextInput
+            inputLabel="Digite sua senha para confirmar"
+            halfWidth
+            value={password}
+            type="password"
+            inputId="password"
+            name="password"
+            onInput={setPassword}
+          />
+        </Container>
+
         <Container row>
           <Button submit>Salvar</Button>
           <Button action="cancel" onClick={handleCancelOperation}>
